23b5db3a5757f259d9966fc1517e155093b878ad
Jared Ryan <jryantennis@gmail.com>
2018-03-31 19:20:13 -0600

    deploy things on heroku

--- index.js
+++ index.js
@@ -5,29 +5,30 @@ const morgan = require('morgan')
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const expressJwt = require('express-jwt');
-const PORT = process.env.PORT || 5000;
+const path = require("path")
+const PORT = process.env.PORT || 8000;
+const secret = process.env.SECRET || "local passphrase spacey goodness";
 
-
+app.use(express.static(path.join(__dirname, "client", "build")))
 app.use(morgan("dev"));
 app.use(bodyParser.json());  // makes it so we can access the request information as req.body in our routes.
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/todo-auth1',
-    {useMongoClient: true}, // helps get rid of deprecation warnings
-    (err) => {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/gamers', (err) => {
         if (err) throw err;
         console.log("Connected to the database");
     }
 );
 
-
 app.use('/api', expressJwt({secret: process.env.SECRET}));  // makes it so we can access the user as req.user in our routes.
-
 app.use('/api/todo', require('./routes/todo'));
 app.use("/auth/change-password", expressJwt({secret: process.env.SECRET}));
 app.use('/auth', require('./routes/auth'));
 app.use('/profile', require('./routes/profile'));
 
+app.get("*", (req, res) => {  
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(PORT, () => {
     console.log(`Server is up on port ${PORT}`)
